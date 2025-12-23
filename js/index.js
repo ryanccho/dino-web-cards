2119be4b3001d05aa896d0ffb9b2eefa6b8bf12e
Ryan Cho <cho.ryan.96@gmail.com>
2025-12-22 19:21:21 -0800

    started dialogue, rearranged layout structure, adjusted surprised duck image, starting rebuild of dialogue control

--- js/index.js
+++ js/index.js
@@ -1,77 +1,90 @@
-let username = "";
-let panels;
-let panelIndex = -1;
+let userName = "";
+let duckName = "";
+let dialogueTree;
+const head = "start";
 
-// get panels from JSON
-const loadPanels = async () => {
-    const response = await fetch("template.json");
-    panels = await response.json();
-    console.log(panels);
+// get dialogue from JSON
+const getDialogue = async () => {
+    const response = await fetch("dialogue.json");
+    dialogueTree = await response.json();
+    console.log(dialogueTree);
 }
 
 // helper function to update elements
 const updateElement = (elementID, value, property) => {
     const element = document.getElementById(elementID);
-    element.style.display = value ? "block" : "none";
+    element.style.display = value ? "block" : "hidden";
     if (value && property) element[property] = value;
 }
 
-// advance a panel
-const advance = () => {
-    panelIndex++;
-    const currentPanel = panels[panelIndex];
-    console.log("Panel: ", panelIndex, currentPanel);
+// render current node
+const renderNode = nodeID => {
+    const currentNode = dialogueTree[nodeID];
+    console.log("Current Node: ", nodeID, currentNode);
+
+    // render text
+    // updateElement("text", currentNode.text, "innerHTML");
+    const dialogueElement = document.getElementById("text");
+    if (currentNode.text) {
+        dialogueElement.innerHTML = `<h1>${currentNode.text}</h1>`;
+        dialogueElement.style.display = "block";
+    } else dialogueElement.style.display = "none";
+    // if (currentNode.insertName) document.getElementById("insert_name").innerText = username;
     
-    // buttonless panel
-    if (panelIndex === 0) document.body.addEventListener("click", advance);
-    else document.body.removeEventListener("click", advance);
+    // render image
+    // updateElement("character_img", `assets/imgs/${currentNode.image}.png`, "src");
+    const imageElement = document.getElementById("character_img");
+    if (currentNode.image) {
+        imageElement.src = `assets/imgs/${currentNode.image}.png`;
+        imageElement.style.display = "block";
+    } else imageElement.style.display = "none";
 
-    // timed panels
-    if (panelIndex === 7 || panelIndex === 8 || panelIndex === 9 || panelIndex === 14) setTimeout(advance, 2500);
-
-    // image
-    updateElement("character_img", currentPanel.image, "src");
-
-    // dialogue
-    updateElement("dialogue", currentPanel.dialogue, "innerHTML");
-    if (currentPanel.insertName) document.getElementById("insert_name").innerText = username;
-
-    // buttons
-    const buttons = document.getElementById("buttons");
-    buttons.innerHTML = "";
-    if (currentPanel.buttons) {
-        currentPanel.buttons.forEach((button, index) => {
-            const buttonElement = document.createElement(button.type);
+    // render buttons
+    const button_container = document.getElementById("button_container");
+    button_container.innerHTML = "";
+    if (currentNode.responses) {
+        currentNode.responses.forEach((button, index) => {
+            const buttonElement = document.createElement("button");
             buttonElement.id = `button${index+1}`;
-            buttonElement.innerHTML = button.value;
-            buttonElement.addEventListener("click", advance);
-            buttons.appendChild(buttonElement);
+            buttonElement.innerHTML = button.label;
+            buttonElement.addEventListener("click", () => renderNode(button.next));
+            button_container.appendChild(buttonElement);
         });
     }
 
     // input
-    updateElement("input", currentPanel.input);
+    // updateElement("input_container", currentNode.input);
+    const inputElement = document.getElementById("input_container");
+    const textInput = document.getElementById("text_input");
+    if (currentNode.input) {
+        inputElement.style.display = "flex";
+        textInput.addEventListener("keydown", event => {
+            if (event.key === "Enter" && textInput.value) {
+                if (currentNode.input === "userName") userName = `${textInput.value}`;
+                else if (currentNode.input === "duckName") duckName = `${textInput.value}`;
+                textInput.value = "";
+                renderNode(currentNode.next);
+            }
+        }, { once: true });
+    } else inputElement.style.display = "none";
+
+    // set timed nodes
+    if (currentNode.timeout) setTimeout(() => renderNode(currentNode.next), currentNode.timeout);
 
     // card
-    updateElement("card", currentPanel.card);
-    document.getElementById("card_name").innerText = username;
-    if (currentPanel.card) document.getElementById("character_container").style.display = "none";
-    else document.getElementById("character_container").style.display = "flex";
+    // updateElement("card", currentNode.card);
+    // document.getElementById("card_name").innerText = username;
+    // if (currentNode.card) document.getElementById("character_container").style.display = "none";
+    // else document.getElementById("character_container").style.display = "flex";
 }
 
 // on page load
 document.addEventListener("DOMContentLoaded", async () => {
-    await loadPanels();
+    await getDialogue();
+    
+    // click anywhere to start
+    document.body.addEventListener("click", () => renderNode(head), { once: true });
+
     document.body.style.display = "block";
     console.log("Page loaded");
-    advance();
-});
-
-// on input
-const usernameInput = document.getElementById("username_input");
-usernameInput.addEventListener("keydown", event => {
-    if (event.key === "Enter" && usernameInput.value) {
-        username = usernameInput.value;
-        advance();
-    }
 });
